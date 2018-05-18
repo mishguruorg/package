ab0b92ed8b6904d35f2fa3b70675c131a52b84f9
George Czabania <george@mish.guru>
2018-05-18 16:58:06 +1200

    beforeAll/afterAll

--- src/ava/index.js
+++ src/ava/index.js
@@ -6,17 +6,21 @@ const { SRC_PATH, TESTS_NAME } = require('../shared/constants')
 
 const BABEL_REGISTER = require.resolve('../babel/register')
 
-const files = await globby(TESTS_NAME, { cwd: SRC_PATH })
-const relativeFiles = files.map((file) => resolve(SRC_PATH, file))
+const start = async () => {
+  const files = await globby(TESTS_NAME, { cwd: SRC_PATH })
+  const relativeFiles = files.map((file) => resolve(SRC_PATH, file))
 
-unwireWithContext('pkg-conf', require.resolve('ava/cli'), () => ({
-  sync: () => ({
-    cache: false,
-    require: [ BABEL_REGISTER ],
-    files: relativeFiles
-  }),
-  filepath: () => SRC_PATH
-}))
+  unwireWithContext('pkg-conf', require.resolve('ava/cli'), () => ({
+    sync: () => ({
+      cache: false,
+      verbose: true,
+      require: [ BABEL_REGISTER ],
+      files: relativeFiles
+    }),
+    filepath: () => SRC_PATH
+  }))
 
-require('ava/cli')
+  require('ava/cli')
+}
 
+start()
--- src/cmd/test.js
+++ src/cmd/test.js
@@ -1,16 +1,26 @@
 /* @flow */
 
+const { join } = require('path')
+const fileExists = require('fileExists')
+
 const exec = require('../shared/exec')
 
 const AVA = require.resolve('../ava')
 
 const test = async () => {
-  await exec('node', [`${process.cwd()}/testHelpers/beforeAll.js`])
+  const beforeAll = join(process.cwd(), '/testHelpers/beforeAll.js')
+  const afterAll = join(process.cwd(), '/testHelpers/afterAll.js')
+
+  if (fileExists(beforeAll)) {
+    await exec('node', [beforeAll])
+  }
 
   const args = process.argv.slice(2)
-  await exec(AVA, args)
+  await exec('node', [AVA, ...args])
 
-  await exec('node', [`${process.cwd()}/testHelpers/afterAll.js`])
+  if (fileExists(afterAll)) {
+    await exec('node', [afterAll])
+  }
 }
 
 module.exports = test
