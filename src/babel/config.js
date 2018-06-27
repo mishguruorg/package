1438eb8c0200fc09fe94a011aa77242053cf1429
George Czabania <george@mish.guru>
2018-06-27 13:17:08 +1200

    Switch to just shorthand-properties

--- src/babel/config.js
+++ src/babel/config.js
@@ -2,17 +2,16 @@
 
 const BABEL_CONFIG = {
   presets: [
-    require.resolve('@babel/preset-flow'),
-
-    // Just for admin-portal, I'd love to remove this preset if possible.
-    require.resolve('@babel/preset-es2015')
+    require.resolve('@babel/preset-flow')
   ],
   plugins: [
     [
       require.resolve('@babel/plugin-proposal-object-rest-spread'),
       { useBuiltIns: true }
     ],
-    require.resolve('@babel/plugin-transform-modules-commonjs')
+    require.resolve('@babel/plugin-transform-modules-commonjs'),
+    // Just for admin-portal, I'd love to remove this preset if possible.
+    require.resolve('@babel/plugin-transform-shorthand-properties')
   ]
 }
 
