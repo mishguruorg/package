5fab258d9502eb8b6e799d61258db4cec9ff2491
George Czabania <george@mish.guru>
2018-06-27 13:08:56 +1200

    Add @babel/preset-es2015

--- src/babel/config.js
+++ src/babel/config.js
@@ -1,7 +1,12 @@
 /* @flow */
 
 const BABEL_CONFIG = {
-  presets: [require.resolve('@babel/preset-flow')],
+  presets: [
+    require.resolve('@babel/preset-flow'),
+
+    // Just for admin-portal, I'd love to remove this preset if possible.
+    require.resolve('@babel/preset-es2015')
+  ],
   plugins: [
     [
       require.resolve('@babel/plugin-proposal-object-rest-spread'),
