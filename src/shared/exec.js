f4954d617c1c09747b1528e51f5b01b69c516b50
George Czabania <george@czabania.com>
2018-07-03 13:12:45 +1200

    Return 1 on exec error (#5)

--- src/shared/exec.js
+++ src/shared/exec.js
@@ -4,12 +4,20 @@ const { spawn } = require('child_process')
 
 const exec = (cmd /* : string */, ...args /* : Array<string> */) => {
   return new Promise((resolve, reject) => {
+    const error = new Error(`"${cmd} ${args.join(' ')}" failed!`)
+
     const child = spawn(cmd, args, {
       stdio: [process.stdin, process.stdout, process.stderr],
       cwd: process.cwd()
     })
 
-    child.on('close', resolve)
+    child.on('close', (code) => {
+      if (code > 0) {
+        reject(error)
+      } else {
+        resolve()
+      }
+    })
     child.on('error', reject)
   })
 }
