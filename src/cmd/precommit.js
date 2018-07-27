16d89904b680eaeb82a827ca0a3cbc41fe36593a
Peter Heesterman <heesterman.peter@gmail.com>
2018-07-27 16:19:54 +1200

    Change the module import

--- src/cmd/precommit.js
+++ src/cmd/precommit.js
@@ -3,14 +3,14 @@
 const exec = require('../shared/exec')
 const { log, fmt } = require('../shared/log')
 
-const LINT_STAGING = require.resolve('lint-staging')
+const LINT_STAGED = require.resolve('lint-staged')
 
 const CONFIG_PATH = require.resolve('../config/lint-staging.json')
 
 const precommit = async () => {
   const args = process.argv.slice(2)
   log(fmt`Running ${'lint-staging'} ${args}`)
-  await exec('node', LINT_STAGING, '--config', CONFIG_PATH, ...args)
+  await exec('node', LINT_STAGED, '--config', CONFIG_PATH, ...args)
 }
 
 module.exports = precommit
