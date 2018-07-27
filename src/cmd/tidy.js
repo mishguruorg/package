1832af73c51dddab105a12f959ce776db22ceb43
Peter Heesterman <heesterman.peter@gmail.com>
2018-07-27 15:17:02 +1200

    configure prettier

--- src/cmd/tidy.js
+++ src/cmd/tidy.js
@@ -7,10 +7,19 @@ const { log, fmt } = require('../shared/log')
 
 const PRETTIER = require.resolve('prettier/bin-prettier')
 
+const config = [
+  '--single-quote',
+  '--trailing-comma',
+  '--no-semi',
+  '--list-different',
+  '--print-width 100',
+  '--write'
+].join(' ')
+
 const prettier = async () => {
   const args = process.argv.slice(2)
   log(fmt`Running ${'prettier'} ${args}`)
-  await exec('node', PRETTIER, '--write', ...args, `**/${SRC_PATH}/**`)
+  await exec('node', PRETTIER, config, ...args, `**/${SRC_PATH}/**`)
 }
 
 module.exports = prettier
