f2e28748e26be65ea15f7f932f9dcb2dafc22236
Peter Heesterman <heesterman.peter@gmail.com>
2018-07-27 15:03:23 +1200

    Simplify tidy command

--- src/cmd/tidy.js
+++ src/cmd/tidy.js
@@ -1,5 +1,6 @@
 /* @flow */
 
+const { SRC_PATH } = require('../shared/constants')
 
 const exec = require('../shared/exec')
 const { log, fmt } = require('../shared/log')
@@ -9,7 +10,7 @@ const PRETTIER = require.resolve('prettier/bin-prettier')
 const prettier = async () => {
   const args = process.argv.slice(2)
   log(fmt`Running ${'prettier'} ${args}`)
-  await exec('node', PRETTIER, ...args)
+  await exec('node', PRETTIER, '--write', ...args, SRC_PATH)
 }
 
 module.exports = prettier
