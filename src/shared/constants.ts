1cf4167b3ed208a471db328b358339047b797573
George Czabania <george@mish.guru>
2019-03-26 23:37:19 +1300

    Add ava-tsc shim

--- src/shared/constants.ts
+++ src/shared/constants.ts
@@ -1,3 +1,3 @@
 export const SRC_PATH = process.env.npm_package_srcPath || 'src'
 export const DIST_PATH = process.env.npm_package_distPath || 'dist'
-export const TESTS_NAME = process.env.npm_package_testsPath || '**/*.spec.js'
+export const TESTS_NAME = process.env.npm_package_testsPath || '**/*.spec.*'
