20e5e4327b8ca1f0f0373db33c0e08263156e420
George Czabania <george@mish.guru>
2019-04-02 06:20:00 +1300

    read-pkg-up instead of using npm env vars

--- src/shared/constants.ts
+++ src/shared/constants.ts
@@ -1,4 +1,8 @@
-export const USE_TSC = process.env.npm_package_types != null
-export const SRC_PATH = process.env.npm_package_srcPath || 'src'
-export const DIST_PATH = process.env.npm_package_distPath || 'dist'
-export const TESTS_NAME = process.env.npm_package_testsPath || '**/*.spec.*'
+import readPkgUp from 'read-pkg-up'
+
+const { pkg } = readPkgUp.sync()
+
+export const USE_TSC = pkg.types != null
+export const SRC_PATH = pkg.srcPath || 'src'
+export const DIST_PATH = pkg.distPath || 'dist'
+export const TESTS_NAME = pkg.testsPath || '**/*.spec.*'
