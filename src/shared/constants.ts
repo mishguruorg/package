789f2b7cfe53829441374429f48568951739deb3
George Czabania <george@mish.guru>
2019-04-16 00:46:05 +1200

    Add exception for types.d.ts

--- src/shared/constants.ts
+++ src/shared/constants.ts
@@ -2,7 +2,7 @@ import readPkgUp from 'read-pkg-up'
 
 const { pkg } = readPkgUp.sync()
 
-export const USE_TSC = pkg.types != null
+export const USE_TSC = pkg.types != null && pkg.types !== 'types.d.ts'
 export const SRC_PATH = pkg.srcPath || 'src'
 export const DIST_PATH = pkg.distPath || 'dist'
 export const TESTS_NAME = pkg.testsPath || '**/*.spec.*'
