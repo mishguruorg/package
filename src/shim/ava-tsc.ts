ff50f40fc98d7474bc31dee201c256e2fc582025
George Czabania <1140167+stayradiated@users.noreply.github.com>
2021-01-10 18:42:05 +1300

    fix(test): mock pkg-conf default async function

--- src/shim/ava-tsc.ts
+++ src/shim/ava-tsc.ts
@@ -7,24 +7,34 @@ import getAVAConfig from '../config/ava-tsc'
 
 console.log('Using AVA with ts-node')
 
+console.dir(pkgConf)
+
 const start = async () => {
-  const avaConfig = await getAVAConfig()
+  const config = await getAVAConfig()
   const filepath = readPkgUp.sync().path
 
-  mockWithContext('pkg-conf', require.resolve('ava/cli'), () => ({
-    sync: (pkgName: string, opts: Record<string, unknown>) => {
-      if (pkgName === 'ava') {
-        return avaConfig
-      }
-      return pkgConf.sync(pkgName, opts)
-    },
-    filepath: (c: Config) => {
-      if (c === avaConfig) {
-        return filepath
-      }
-      return pkgConf.filepath(c)
-    },
-  }))
+  const mockedPkgConf = async (
+    pkgName: string,
+    opts: Record<string, unknown>,
+  ) => {
+    return mockedPkgConf.sync(pkgName, opts)
+  }
+
+  mockedPkgConf.sync = (pkgName: string, opts: Record<string, unknown>) => {
+    if (pkgName === 'ava') {
+      return config
+    }
+    return pkgConf.sync(pkgName, opts)
+  }
+
+  mockedPkgConf.filepath = (c: Config) => {
+    if (c === config) {
+      return filepath
+    }
+    return pkgConf.filepath(c)
+  }
+
+  mockWithContext('pkg-conf', require.resolve('ava/cli'), () => mockedPkgConf)
 
   return require('ava/cli')
 }
